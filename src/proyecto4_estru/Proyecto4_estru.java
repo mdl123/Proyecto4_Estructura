f12974f404bd88b16c28ad1f010815286c1553fb
DenisseCar <denisse.suazo@gmail.com>
2014-03-16 21:36:41 -0600

    Actualizacion

--- src/proyecto4_estru/Proyecto4_estru.java
+++ src/proyecto4_estru/Proyecto4_estru.java
@@ -19,6 +19,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
@@ -33,61 +34,79 @@ public class Proyecto4_estru {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        ArrayList<Clase> clases = new ArrayList();
-        File archivo = null;
-        FileReader canal = null;
-        BufferedReader buffer = null;
-        try {
-            archivo = new File("./SistemasComputacionales.txt");
-            canal = new FileReader(archivo);
-            buffer = new BufferedReader(canal);
-            String linea;
-            try {
-                while(buffer.readLine()!= null){
-                    linea = buffer.readLine();
-                    String[] tokens = linea.split(",");
-                    String cod = tokens[0];
-                    String name = tokens[1];
-                    ArrayList<Clase> requisito = new ArrayList();
-                    for(int i=2;i<tokens.length;i++)
-                        for(int j=0;j<clases.size();j++)
-                            if(tokens[i].equals(clases.get(j).getCod()))
-                                requisito.add(clases.get(j));
-                    
-                    clases.add(new Clase(cod,name,requisito));
-                    
-                }
-            } catch (Exception e) {
-            }
-       } catch (Exception k) {
-       }
-       try{
-            buffer.close();
-            canal.close();
-       }catch(Exception h){
-       }
-      
-       for(int i=0;i<clases.size();i++)
-            System.out.println(clases.get(i));
+        Scanner s=new Scanner(System.in);
     
-       
-
         //hay ponerle algo a las aristas como ruta1, ruta 2 , etc
         Graph<String, String> g = new DirectedSparseMultigraph<String, String>();
-  
-        g.addEdge("ruta1","g" ,"x");
-        g.addEdge("ruta2","g" ,"y");
-        g.addEdge("ruta3","v" ,"y");
-        g.addEdge("ruta4","x" ,"z");
-        ArrayList<nodo> topo = toposort(g);
-        System.out.println(topo.size());
-        for(int i=0;i<topo.size();i++){
-            System.out.println("Nodo Actual: "+topo.get(i).label);
-            for (int i2 = 0; i2 < topo.get(i).ar.size(); i2++) {
-                System.out.println("Requerido para "+topo.get(i).label+ " "+ topo.get(topo.get(i).ar.get(i2)).label);
-            }
-        }//fin del for
+        menu();
+        String entrada=s.nextLine();
+        String carreraSeleccionada = null;
+        ArrayList<Clase> clases = new ArrayList();
+        do{
+                if("1".equals(entrada)){
+                    System.out.println("Ingrese el numero de la carrera");
+                    System.out.println("");
+                    menu2();
+                    entrada=s.nextLine();
+                    int valor= Integer.parseInt(entrada);
+                    if(valor<1||valor>6||entrada.length()>1){
+                        System.out.println("Valor Incorrecto"); 
+                        System.out.println("");
+                    }//fin del if
+                    else{
+                        switch(valor){
+                            case 1: carreraSeleccionada = "./SistemasComputacionales.txt";
+                            case 2: carreraSeleccionada = "./AdministracionEmpresasTuristicas.txt";
+                            case 3: carreraSeleccionada = "./ComunicacionPublicidad.txt";
+                            case 4: carreraSeleccionada = "./Biomedica.txt";
+                            case 5: carreraSeleccionada = "./Industrial.txt";
+                            case 6: carreraSeleccionada = "./Finanzas.txt";
+                            default: carreraSeleccionada = "";
+                        }
+                        File archivo = null;
+                        BufferedReader buffer = null;
+                        try {
+                             archivo = new File(carreraSeleccionada);
+                             buffer = new BufferedReader(new FileReader(archivo));
+                             String linea;
+                             try {
+                                while(buffer.ready()){
+                                    linea = buffer.readLine();
+                                    String[] tokens = linea.split(",");
+                                    String cod = tokens[0];
+                                    String name = tokens[1];
+                                    ArrayList<Clase> requisito = new ArrayList();
+                                    for(int i=2;i<tokens.length;i++)
+                                        for(int j=0;j<clases.size();j++)
+                                             if(tokens[i].equals(clases.get(j).getCod()))
+                                                requisito.add(clases.get(j));
+                    
+                                    clases.add(new Clase(cod,name,requisito));
+                    
+                                }
+                            } catch (Exception e) {
+                            }
+                       } catch (Exception k) {
+                       }
+                       try{
+                            buffer.close();
+                       }catch(Exception h){
+                       }
+                       execute(g);
+                   }
+                 }            
+                       
+                 menu();
+                 entrada=s.nextLine();
+        }while(!"2".equals(entrada));
         
+       
+       
+      
+  }//fin del main
+    
+     public static void execute(Graph g){
+        ArrayList<nodo> topo = toposort(g);
         Layout l = new FRLayout(g);
         BasicVisualizationServer<String, String> vv = new BasicVisualizationServer<String, String>(l);
         vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
@@ -100,12 +119,48 @@ public class Proyecto4_estru {
         frame.getContentPane().add(vv);
         frame.pack();
         frame.setVisible(true);
- }//fin del main
+       //fin del ejemplo
+        
+        //codigo principal
+        ArrayList<nodo>no=new ArrayList();
+        for(int i=0;i<topo.size();i++){
+            if(topo.get(i).ar.isEmpty()){
+                System.out.print(topo.get(i).label+" ");
+                no.add(topo.get(i));
+            }//fin del if
+        }//fin del for
+        boolean marca=false;
+        System.out.println("");
+        ArrayList<nodo>meter=new ArrayList();
+        ArrayList<Integer>in=new ArrayList();
+        ArrayList<String>impresion=new ArrayList();
+        for(int i3=0;i3<no.size();i3++){
+            for(int i=0;i<topo.size();i++){
+                for(int i2=0;i2<topo.get(i).ar.size();i2++){
+                    if(no.isEmpty()==false){
+                         if(no.contains(topo.get(topo.get(i).ar.get(i2)))){
+                            if(meter.contains(topo.get(i))==false){
+                                System.out.print(topo.get(i).label+" ");
+                                meter.add(topo.get(i));
+                            }
+                        }//fin del if
+                    }//fin del if
+               }//fin del for
+         
+            }//fin del for
+            no.clear();
+            for(int i4=0;i4<meter.size();i4++){
+                no.add(meter.get(i4));
+            }
+            meter.clear();
+            System.out.println("");
+       }//fin del tercer for
+    }
+    
     
     //orden topologico
     public static ArrayList<nodo> toposort(Graph g){
-        ArrayList<nodo>st;
-        st=new ArrayList();
+        ArrayList<nodo>st=new ArrayList();
         Collection vertices = g.getVertices();
         Object[] vertex = vertices.toArray();
         for(int i=0;i<vertex.length;i++){
@@ -115,17 +170,17 @@ public class Proyecto4_estru {
         for(int i=0;i<vertex.length;i++){
             Collection inEdges = g.getInEdges(vertex[i].toString());
             Object[] In = inEdges.toArray();
-            for(int i2=0;i2<In.length;i2++){
-                Pair endpoints = g.getEndpoints(In[i2].toString());
-                if(!endpoints.getFirst().toString().equals(st.get(i).label)){
-                    int x=finder(st,endpoints.getFirst().toString());
-                    st.get(i).ar.add(x);
-                }//fin del if
-                if(!endpoints.getSecond().toString().equals(st.get(i).label)){
-                    int x=finder(st,endpoints.getSecond().toString());
-                    st.get(i).ar.add(x);
-                }//fin del if
-           }//fin del segundo for
+                for(int i2=0;i2<In.length;i2++){
+                    Pair endpoints = g.getEndpoints(In[i2].toString());
+                    if(!endpoints.getFirst().toString().equals(st.get(i).label)){
+                        int x=finder(st,endpoints.getFirst().toString());
+                        st.get(i).ar.add(x);
+                    }//fin del if
+                    if(!endpoints.getSecond().toString().equals(st.get(i).label)){
+                        int x=finder(st,endpoints.getSecond().toString());
+                        st.get(i).ar.add(x);
+                     }//fin del if
+                }//fin del segundo for
               
         }//fin del for
        
@@ -133,6 +188,7 @@ public class Proyecto4_estru {
     }
     
     public static int finder(ArrayList<nodo>ar, String x){
+      
         for(int i=0;i<ar.size();i++){
             if(ar.get(i).label == null ? x == null : ar.get(i).label.equals(x)){
                 return i;
@@ -141,4 +197,23 @@ public class Proyecto4_estru {
         return -1;
     }
     
+    
+    public static void menu(){
+        System.out.println("");
+        System.out.println("Menu principal");
+        System.out.println("1.-Ingresar Carrera");
+        System.out.println("2.-Salir");
+    }
+    
+    public static void menu2(){
+        System.out.println("");
+        System.out.println("Escoja su carrera a continuacion:");
+        System.out.println("1.-Ingeniera en Sistemas Computacionales");
+        System.out.println("2.-Administracion de Empresas Turisticas");
+        System.out.println("3.-Comunicacion y Publicidad");
+        System.out.println("4.-Biomedica");
+        System.out.println("5.-Ingeniera Industrial");
+        System.out.println("6.-Finanzas");
+       
+    }
 }//fin de la clase
